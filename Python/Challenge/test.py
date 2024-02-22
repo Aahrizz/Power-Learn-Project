adbae76962fe2150a183ae140741b6da2f6c4708
Aahrizz <upjeffrey@outlook.com>
2024-02-22 15:12:49 +0300

    new test

---
+++ Python/Challenge/test.py
@@ -0,0 +1,12 @@
+#initializes an empty dict
+x = {}
+
+#key value pair. The key being 2 and its value mapped as 10
+x[2] = 10
+
+#key value pair. The key being 1, mapped to a value of list
+x[1] = [20,30,40]
+
+#our output is the value of the second index of the key 1
+print(x[1][2])
+
