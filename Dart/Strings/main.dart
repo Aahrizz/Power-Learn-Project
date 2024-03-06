719a49f988a8a6fcdfd1370464fd0b4001d91c60
Aahrizz <upjeffrey@outlook.com>
2024-03-06 09:09:14 +0300

    09:09

---
+++ Dart/Strings/main.dart
@@ -0,0 +1,6 @@
+void main() {
+    String schoolName = "Powerlearn Project Academy";
+    String address = "AFrica";
+
+    print("My School name is $schoolName and the address is $address");    
+}
